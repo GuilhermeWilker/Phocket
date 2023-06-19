396e36a5557ce92ca11e4604b195e62722a9a92f
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-06-19 10:38:08 -0300

    chore: project restructuring

---
+++ public/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="<?php echo $BASE_URL; ?>/assets/css/style.css"
+    />
+
+    <style>
+      body {
+        font-family: "Ubuntu Mono", monospace;
+      }
+
+      input {
+        font-family: "Ubuntu Mono", monospace;
+        font-size: 14px;
+      }
+    </style>
+    <title>Phocket</title>
+  </head>
+  <body>
+    <main>
+      <h3>Olá</h3>
+  </body>
+</html>
